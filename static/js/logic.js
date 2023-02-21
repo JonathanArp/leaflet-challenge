c7b27d994abf1f3a07ffec255e4273601588eaf1
Jonathan_Arp <jonathan.arp14@gmail.com>
2023-02-21 10:30:01 -0600

    pseudo code

---
+++ static/js/logic.js
@@ -0,0 +1,11 @@
+// Define function to create map layers and display them
+
+// Define function to assign colors based on earthquake depth
+
+// Define function to create markers based on JSON response objects
+
+// URL to get Earthquake data from USGS.gov site
+
+// URL to get tectonic plate data from Hugo Ahlenius, Nordpil and Peter Bird GitHub page
+
+// DataPromise to get geoJSON data from urls, then call functions to create the markers & map
